aea1e334f732e2e19a9f2bfba8f7d4f88851b0fb
iusgiuwrugiav345 <matersasm@gmail.com>
2026-03-30 15:10:59 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+TOKEN = "8588396631:AAH3Vs064g_9esWe09EO8TeNVmLRLmZRQ6c"
+DB_NAME_USERS = "users.db"
+DB_NAME_APPS = "apps.db"
+
+# Лимиты
+FREE_DOWNLOADS = 5  # Сколько раз можно скачать бесплатно
+PRICE = 0.60  # Цена подписки (например, в USDT эквиваленте)
+
+# URL ????-???? (Telegram Web App)
+WEBAPP_URL = ""https://boostixnet.onrender.com/webapp?v=5""
+
+# API-ключи для оплаты
+CRYPTOBOT_API = "451092:AAqI0nRURGaWhW5rHy0rFk3T6z6r1wYS87U"
+
+ADMINS = [7120639769]  # Замени на свой Telegram ID
+ADMIN_CHAT_ID = ADMINS[0]  # ???? ????? ??????? (????? ???????? ?? ???/?????)
+
+# Photo ID for premium subscription message
+PREMIUM_PHOTO_ID = "attached_assets/IMG_3562.jpeg"
